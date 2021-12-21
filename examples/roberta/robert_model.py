347c04fb896434a3a7948c42245c9496342bdd21
Qingsong Lv <lqs@mail.bnu.edu.cn>
2021-12-21 15:03:13 +0800

    modify gelu

--- examples/roberta/robert_model.py
+++ examples/roberta/robert_model.py
@@ -2,17 +2,8 @@ import torch
 import torch.nn as nn
 from SwissArmyTransformer.mpu.transformer import BaseTransformer, SelfAttention, CrossAttention, MLP, LayerNorm
 from SwissArmyTransformer.model.base_model import BaseMixin, BaseModel
-import math
 
-def roberta_gelu(x):
-    """
-    from https://github.com/huggingface/transformers/blob/master/src/transformers/activations.py#L27
-    Original Implementation of the GELU activation function in Google BERT repo when initially created. For
-    information: OpenAI GPT's GELU is slightly different (and gives slightly different results): 0.5 * x * (1 +
-    torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * torch.pow(x, 3)))) This is now written in C in nn.functional
-    Also see the Gaussian Error Linear Units paper: https://arxiv.org/abs/1606.08415
-    """
-    return x * 0.5 * (1.0 + torch.erf(x / math.sqrt(2.0)))
+roberta_gelu = nn.functional.gelu
 
 class RobertaTransformerLayer(torch.nn.Module):
     def __init__(
