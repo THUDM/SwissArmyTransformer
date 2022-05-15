13c6da703499306876be982a8c2472c4204b6590
Qingsong Lv <lqs@mail.bnu.edu.cn>
2022-05-15 20:25:52 +0800

    ViT automatic download

--- examples/vit/config/model_vit_224_16_21k.sh
+++
@@ -1,4 +0,0 @@
-MODEL_TYPE="vit-base-224-16-21k"
-MODEL_ARGS="--load ${CHECKPOINT_PATH}/swiss-vit-base-patch16-224-in21k \
-            --num-finetune-classes 10 \
-            --image-size 384 384"
--- examples/vit/scripts/finetune_cifar10.sh
+++ examples/vit/scripts/finetune_cifar10.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
 # Change for multinode config
-CHECKPOINT_PATH=/data/qingsong/pretrain/
+CHECKPOINT_PATH=$1
+if [[ "$1" == "" ]] || [[ "$2" == "" ]];
+then
+    echo "Please pass in two root folders to save model and data!"
+    exit
+fi
 
 NUM_WORKERS=1
 NUM_GPUS_PER_WORKER=8
@@ -10,14 +15,26 @@ MP_SIZE=1
 script_path=$(realpath $0)
 script_dir=$(dirname $script_path)
 main_dir=$(dirname $script_dir)
-source $main_dir/config/model_vit_224_16_21k.sh
+MODEL_TYPE="swiss-vit-base-patch16-224-in21k"
+if [ ! -d "${CHECKPOINT_PATH}/$MODEL_TYPE" ]
+then
+    if [ ! -f "${CHECKPOINT_PATH}/$MODEL_TYPE.zip" ]
+    then
+        wget "https://cloud.tsinghua.edu.cn/f/1381d0995d5e4634856e/?dl=1" -O "${CHECKPOINT_PATH}/$MODEL_TYPE.zip"
+    fi
+    unzip "${CHECKPOINT_PATH}/$MODEL_TYPE.zip" -d "${CHECKPOINT_PATH}"
+fi
+MODEL_ARGS="--load ${CHECKPOINT_PATH}/$MODEL_TYPE \
+            --num-finetune-classes 10 \
+            --image-size 384 384"
+
 
 OPTIONS_NCCL="NCCL_DEBUG=info NCCL_IB_DISABLE=0 NCCL_NET_GDR_LEVEL=2"
 HOST_FILE_PATH="hostfile"
 HOST_FILE_PATH="hostfile_single"
 
-en_data="/data/qingsong/dataset/train"
-eval_data="/data/qingsong/dataset/test"
+en_data="$2/train"
+eval_data="$2/test"
 
 
 config_json="$script_dir/ds_config_ft.json"
@@ -52,7 +69,7 @@ gpt_options="${gpt_options}
 "
               
 
-run_cmd="${OPTIONS_NCCL} deepspeed --master_port 16666 --num_nodes ${NUM_WORKERS} --num_gpus ${NUM_GPUS_PER_WORKER} --hostfile ${HOST_FILE_PATH} finetune_vit_cifar10.py $@ ${gpt_options}"
+run_cmd="${OPTIONS_NCCL} deepspeed --master_port 16666 --num_nodes ${NUM_WORKERS} --num_gpus ${NUM_GPUS_PER_WORKER} --hostfile ${HOST_FILE_PATH} finetune_vit_cifar10.py ${gpt_options}"
 echo ${run_cmd}
 eval ${run_cmd}
 
