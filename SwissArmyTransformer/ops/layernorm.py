6eccd8b292dc1a1afee2177e8802857e100cde87
Ming Ding <dm_thu@qq.com>
2022-06-16 02:33:53 -0400

    make apex optional

--- SwissArmyTransformer/ops/layernorm.py
+++ SwissArmyTransformer/ops/layernorm.py
@@ -1,11 +1,14 @@
-from apex.normalization.fused_layer_norm import FusedLayerNorm
-class LayerNorm(FusedLayerNorm):
-    def __init__(self, *args, pb_relax=False, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.pb_relax = pb_relax
+try:
+    from apex.normalization.fused_layer_norm import FusedLayerNorm
+    class LayerNorm(FusedLayerNorm):
+        def __init__(self, *args, pb_relax=False, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.pb_relax = pb_relax
 
-    def forward(self, x):
-        if not self.pb_relax:
-            return super().forward(x)
-        return super().forward(x / (x.abs().max().detach() / 8))
-# from  torch.nn import LayerNorm
+        def forward(self, x):
+            if not self.pb_relax:
+                return super().forward(x)
+            return super().forward(x / (x.abs().max().detach() / 8))
+except ModuleNotFoundError:
+    print('Please install apex to use fused_layer_norm, fall back to torch.nn.LayerNorm')
+    from  torch.nn import LayerNorm
