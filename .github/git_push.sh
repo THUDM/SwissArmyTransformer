321ad2bf82338b9fb6caf391be8f5ffc464c9a84
git <you@example.com>
2022-07-02 11:50:24 +0800

    update workflow

--- .github/git_push.sh
+++ .github/git_push.sh
@@ -1,7 +1,7 @@
 if [[ `git status --porcelain` ]]; then
-	git config --global user.email "lqs@mail.bnu.edu.cn"
-	git config --global user.name "Qingsong Lv"
-	git add README.md
-	git commit -m "index file by github actions"
-	git push origin docs
+    git config --global user.email "lqs@mail.bnu.edu.cn"
+    git config --global user.name "Qingsong Lv"
+    git add README.md
+    git commit -m "index file by github actions"
+    git push origin docs
 fi
