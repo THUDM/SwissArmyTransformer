a82e27a85b281282215d1807f42dce0656e77dd8
吕青松 <lvqingsong@lvqingsongdeMacBook-Pro.local>
2023-07-18 14:35:10 +0800

    remove unnecessary dependency

--- sat/ops/__init__.py
+++ sat/ops/__init__.py
@@ -12,8 +12,6 @@ import sys
 import math
 import random
 
-from .layernorm import LayerNorm
-from .fused_ema_adam import FusedEmaAdam
 # dynamic import according to name with importlib
 from importlib import import_module
 
