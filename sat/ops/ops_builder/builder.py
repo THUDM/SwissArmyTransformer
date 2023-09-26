bd333dd063b96da195c157b0c3f5cca7ce95be5d
zhaolei <lei.zhao@aminer.cn>
2023-09-26 15:12:21 +0800

    fast-rotary-embedding

--- sat/ops/ops_builder/builder.py
+++ sat/ops/ops_builder/builder.py
@@ -631,7 +631,7 @@ class CUDAOpBuilder(OpBuilder):
         if sys.platform == "win32":
             return ['-O2']
         else:
-            return ['-O3', '-std=c++14', '-g', '-Wno-reorder']
+            return ['-O3', '-std=c++17', '-g', '-Wno-reorder']
 
     def nvcc_args(self):
         if self.build_for_cpu:
