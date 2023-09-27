fix precision problem

sat/model/finetune/lora2.py
@@ -160,7 +160,7 @@ def merge_linear_lora(lin):
     guess_type = lin.original.bias.data.dtype if lin.original.bias is not None else lin.original.weight.data.dtype
     if guess_type is torch.uint8:
         guess_type = torch.float32
-    new_lin.weight.data = weight + new_qkv.T.to(guess_type)
+    new_lin.weight.data = (weight + new_qkv.T).to(guess_type)
     return new_lin.cuda() if torch.cuda.is_available() else new_lin
 
 class LoraMixin(BaseMixin):

sat/model/position_embedding/triton_rotary_embeddings.py
@@ -201,9 +201,7 @@ class FastRotaryEmbedding(torch.nn.Module):
             else:
                 t = torch.arange(seqlen, device=device, dtype=self.inv_freq.dtype)
                 inv_freq = self.inv_freq
-            # Don't do einsum, it converts fp32 to fp16 under AMP
-            #freqs = torch.einsum("i,j->ij", t, self.inv_freq)
-            freqs = torch.outer(t, inv_freq)
+            freqs = torch.einsum("i,j->ij", t, inv_freq)
             if self.scale is None:
                 self._cos_cached = torch.cos(freqs).to(dtype)
                 self._sin_cached = torch.sin(freqs).to(dtype)